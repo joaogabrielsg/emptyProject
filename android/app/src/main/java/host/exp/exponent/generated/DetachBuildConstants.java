192f2fe2812802d312bd292a3c5c647ecf0ea019
joaogabrieldosantosgoncalves@gmail.com <joaogabrieldosantosgoncalves@gmail.com>
2019-11-27 17:29:03 -0300

    Initial commit

---
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -0,0 +1,8 @@
+package host.exp.exponent.generated;
+
+// This file is auto-generated. Please don't rename!
+public class DetachBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "exp022d4443dabb4a50ab30e6d03fcb8768://192.168.0.19:80";
+
+}
